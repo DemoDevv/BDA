f98433dca28fc090684a706aafc7fcec683ff036
DemoDevv <mathieulebras95@gmail.com>
2024-04-22 18:36:42 +0200

    premier event pour verifier si le bot fonctionne

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits, Events } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Logged in as ${readyClient.user!.tag}`);
+});
+
+client.login(Bun.env.TOKEN);
