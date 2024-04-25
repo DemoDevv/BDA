7c4a21a168d7276a80ad83ba583e4fdbf66940e9
DemoDevv <mathieulebras95@gmail.com>
2024-04-25 10:56:09 +0200

    ajout d'un timeout pour attendre le début d'une semaine pour qu'il soit bien synchro

--- src/workers/feuille-worker.ts
+++ src/workers/feuille-worker.ts
@@ -7,6 +7,7 @@ import type Client from "../structs/client";
 
 export default class FeuilleWorker extends Worker {
   public interval: Timer | null = null;
+  public timeout: Timer | null = null;
   public URL_SCHEDULE = "https://edt.univ-nantes.fr/iut_nantes/g191826.xml";
   public browser: Browser | null = null;
 
@@ -23,7 +24,6 @@ export default class FeuilleWorker extends Worker {
   }
 
   async start(): Promise<void> {
-    console.log("Feuille worker started!");
     if (this.client.config.CHROME_BIN) {
       this.browser = await puppeteer.launch({
         executablePath: this.client.config.CHROME_BIN,
@@ -32,12 +32,25 @@ export default class FeuilleWorker extends Worker {
     } else {
       this.browser = await puppeteer.launch();
     }
-    this.interval = setInterval(
-      () => {
-        this.execute();
-      },
-      this.client.config.ENV == "DEV" ? 5000 : 7 * 24 * 60 * 60 * 1000,
-    );
+    // wait the sunday at 00:00 to start the worker
+    const now = new Date();
+    const timeToWait =
+      7 * 24 * 60 * 60 * 1000 -
+      (now.getDay() * 24 * 60 * 60 * 1000 +
+        now.getHours() * 60 * 60 * 1000 +
+        now.getMinutes() * 60 * 1000 +
+        now.getSeconds() * 1000 +
+        now.getMilliseconds());
+    console.log(`Waiting ${timeToWait}ms to start the worker`);
+    this.timeout = setTimeout(() => {
+      console.log("[Feuille Worker] Starting the worker");
+      this.interval = setInterval(
+        () => {
+          this.execute();
+        },
+        this.client.config.ENV == "DEV" ? 5000 : 7 * 24 * 60 * 60 * 1000,
+      );
+    }, timeToWait);
   }
 
   async getCurrentWeek(): Promise<number> {
@@ -83,6 +96,7 @@ export default class FeuilleWorker extends Worker {
 
   async stop(): Promise<void> {
     clearInterval(this.interval!);
+    clearTimeout(this.timeout!);
     await this.browser?.close();
   }
 }
