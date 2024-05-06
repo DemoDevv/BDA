0a1ad7557896dbfc5a8d6de47ce1b31a2e08872e
DemoDevv <mathieulebras95@gmail.com>
2024-05-06 12:05:03 +0200

    mise a jour des dépendances pour patch un bug de puppeteer

--- src/helpers/get-schedule.ts
+++ src/helpers/get-schedule.ts
@@ -4,14 +4,19 @@ export default async (
   browser: Browser,
   url: string | undefined,
 ): Promise<Buffer | undefined> => {
-  const page = await browser.newPage();
-  page?.setViewport({ width: 1000, height: 1000 });
-  if (!url) throw new Error("URL_SCHEDULE is not defined!");
-  await page?.goto(url);
-  const scheduleTable = await page?.$(
-    'body > span[style*="display: inline"][id]',
-  );
-  const scheduleBuffer = await scheduleTable?.screenshot();
-  await page?.close();
-  return scheduleBuffer;
+  try {
+    const page = await browser.newPage();
+    page.setViewport({ width: 1000, height: 1000 });
+    if (!url) throw new Error("URL_SCHEDULE is not defined!");
+    await page.goto(url);
+    const scheduleTable = await page.$(
+      'body > span[style*="display: inline"][id]',
+    );
+    const scheduleBuffer = await scheduleTable?.screenshot();
+    await page.close();
+    return scheduleBuffer;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
 };
