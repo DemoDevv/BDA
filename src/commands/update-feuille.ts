b408a36533c9b0f218d1ab30d9073a2ed427a514
DemoDevv <mathieulebras95@gmail.com>
2024-05-13 10:15:53 +0200

    création de la structure de base pour la commande

---
+++ src/commands/update-feuille.ts
@@ -0,0 +1,14 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import type Client from "../structs/client";
+
+import { RegisteredWorker } from "../workers";
+import type FeuilleWorker from "../workers/feuille-worker";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("update-feuille")
+    .setDescription("Update the feuille manually."),
+  async execute(_: ChatInputCommandInteraction, client: Client): Promise<void> {
+    const worker = client.getWorker(RegisteredWorker.FEUILLE) as FeuilleWorker;
+  },
+};
