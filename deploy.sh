48a99857b31ade5e46c13ba8c25eae34c136c8c7
DemoDevv <mathieulebras95@gmail.com>
2024-05-14 21:13:31 +0200

    changement dans le dockerfile pour régler le soucis du dossier data et ajout dans le script de déploiement

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+
+# Check if Docker container named 'bda' exists
+container_exists=$(docker ps -a | grep bda | wc -l)
+
+# Check if Docker image named 'bda-image' exists
+image_exists=$(docker images | grep bda-image | wc -l)
+
+# Remove container and image if both exist
+if [ $container_exists -gt 0 ] && [ $image_exists -gt 0 ]; then
+  echo "Removing container 'bda' and image 'bda-image'..."
+  docker rm -f bda
+  docker rmi bda-image
+else
+  echo "Container or image doesn't exist. Skipping removal."
+fi
+
 mkdir data
+chmod u+rw data
 docker build --pull -t bda-image .
-docker run -v ./data:/usr/src/app/data bda-image
+docker run --name bda -v ./data:/usr/src/app/data bda-image
